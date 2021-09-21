415a7a29671aeb8d5411bbb4ed304d80ee2feb3b
Tim <timyoo25@gmail.com>
2021-09-21 11:21:52 -0600

    test seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Blog.destroy_all
 User.destroy_all
+Category.destroy_all
 
 @admin = User.create!(username: 'timkev', email: 'sparrow@email.com', password: '123456')
 @admin1 = User.create!(username: 'kev', email: 'kev@email.com', password: '123456')
@@ -13,3 +16,16 @@ User.destroy_all
 @admin3 = User.create!(username: 'christian', email: 'christian@email.com', password: '123456')
 
 puts "#{User.count} user(s) created"
+
+@personal_finance = Category.create!(name: "Personal Finance")
+@covid_impact = Category.create!(name: "COVID-Impact")
+@news_events = Category.create!(name: "News Events")
+@financial_calculator = Category.create!(name: "Finance Calculator")
+
+puts "#{Category.count} categories created"
+
+@test_blog = Blog.create!(title: "test", picture: "URL", body: "this is a test", user: @admin, category: @personal_finance)
+
+puts "#{Blog.count} blogs created"
+
+@personal_finance.blogs << @test_blog
