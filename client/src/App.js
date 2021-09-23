941d2c1e11fdfb4cf1249c3ec4799a8b93c67243
Kevin Wand <kevinwand@Kevins-Air.home>
2021-09-23 14:30:35 -0400

    initial start on layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react'
+import { Route, Switch, useHistory } from 'react-router-dom'
+import {
+	loginUser,
+	registerUser,
+	removeToken,
+	verifyUser,
+} from './services/auth';
 
 function App() {
+  const [currentUser, setCurrentUser] = useState(null);
+	const history = useHistory();
+
+	useEffect(() => {
+		const handleVerify = async () => {
+			const userData = await verifyUser();
+			setCurrentUser(userData);
+		};
+		handleVerify();
+	}, []);
+
+	const handleLogin = async (formData) => {
+		const userData = await loginUser(formData);
+		setCurrentUser(userData);
+		history.push('/');
+	};
+
+	const handleRegister = async (formData) => {
+		const userData = await registerUser(formData);
+		setCurrentUser(userData);
+		history.push('/');
+	};
+
+	const handleLogout = () => {
+		setCurrentUser(null);
+		localStorage.removeItem('authToken');
+		removeToken();
+		history.push('/');
+	};
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* 
+      Body Layout wrapping entire web app
+      
+      Routes for user auth & blogs laid out here:
+        Route for login
+        Route for sign up
+        Route for blogs & categories
+      
+      */}
+
     </div>
   );
 }
---
+++ client/src/layouts/Layout.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Layout(props) {
+  const { currentUser, handleLogout } = props
+  return (
+    <div>
+
+    </div>
+  )
+}
