b2e9944132f91a99bc5c0e32ea6ba5c20c317cca
Kevin Wand <kevinwand@Kevins-Air.home>
2021-09-23 13:25:35 -0400

    back end up and running

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,5 @@
 class CategoriesController < ApplicationController
-  before_action :set_category, only: [:show, :update, :destroy]
+  before_action :set_category, only: [:show]
 
   # GET /categories
   def index
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,12 @@ Rails.application.routes.draw do
   # resources :categories, only: :index
   resources :blogs # do resources :comments
   resources :users, only: :create
+
+  concern :blog do
+    resources :blogs
+  end
+
+  resources :categories, concerns: :blog
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/categories/:category_id/blogs', to: 'blogs#get_by_category'
+  # get '/categories/:category_id/blogs', to: 'blogs#get_by_category'
 end
